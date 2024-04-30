428bf2e79768a0cd6348351ff4e8e6e9cf441121
FrizzyDex <168575596+FrizzyDex@users.noreply.github.com>
2024-04-30 20:56:23 +0300

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,17 @@
+from aiogram import executor, types, Bot, Dispatcher
+
+token = '7066702701:AAFbr0iviTtnLDcFmd4asckiyNc6o9CQxxE'
+bot = Bot(token=token)
+dp = Dispatcher(bot=bot)
+import asyncio
+
+@dp.message_handler(commands=['start'])
+async def echo_handler(message: types.Message):
+    await message.answer(f'Приветствую, {message.from_user.username}! Это бот от Антона. Вот список команд, которые в данный момент поддерживает бот: /about')
+
+@dp.message_handler(commands=['about'])
+async def echo_handler(message: types.Message):
+    await message.answer('Данный бот создан для всего, что только можно сюда впихнуть. Спасибо за внимание!')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
